47bad9bbe809047b3ecbd0613a218337815b7310
NagyFeri <cybers.prog.tech@gmail.com>
2022-04-19 20:14:33 +0200

    ts teglalap hozzafuzve

---
+++ egyszeruValtozok.ts
@@ -0,0 +1,23 @@
+// hatáskör valtozoNev :tipus = érték;
+var szam:number = 5;
+var szoveg:string="Szeretem a programozást";
+var logikai:boolean = false;//vagy true értéket vehet fel
+
+//Speciális változók
+
+// :any
+
+var barmi:any="alma";
+barmi=5;
+barmi=true;
+document.write(barmi);
+
+var ismeretlen:unknown="alma"
+ismeretlen=5;
+ismeretlen=true;
+document.write(String(ismeretlen));
+
+
+var szovegesTomb1: string[] =["alma", "körte", "szilva", "barack"];
+var szovegesTomb2: Array<string>=["fps","tps","rts","szimulátor"];
+var szamTomb: Array<number>=[3,7,13,21,42];
